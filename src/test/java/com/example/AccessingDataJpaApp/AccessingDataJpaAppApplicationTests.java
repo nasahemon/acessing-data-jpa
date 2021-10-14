0a6e8a22baf90b049e4e620ea21843833c172cd8
nasah <nasahemon@gmail.com>
2021-10-14 15:30:26 +0600

    initial commit

---
+++ src/test/java/com/example/AccessingDataJpaApp/AccessingDataJpaAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.AccessingDataJpaApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccessingDataJpaAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
